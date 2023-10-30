b96f67ff3e7ed69b76425338854a2c9eeaab91d4
LincolnK12 <147334624+LincolnK12@users.noreply.github.com>
2023-10-30 17:34:08 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-
-
 $(function() {
+    var firstScroll = false;
+
     $(window).on("scroll", function() {
+        // Your existing header code
         if($(window).scrollTop() > 50) {
             $(".header").addClass("active");
         } else {
-           $(".header").removeClass("active");
+            $(".header").removeClass("active");
+        }
+
+        // Code to hide video and show new content only on the first scroll
+        if(!firstScroll && $(window).scrollTop() > 0) {
+            $('.back-video').hide(); // Hides the video section
+            $('.home2').hide();
+            $('#new-content').show();    // Shows the new content
+            firstScroll = true;          // Set the flag to true after first scroll
         }
     });
 });
+
+
+

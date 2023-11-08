75a4e316a0bf647b6b858873ae02ea51403c8ef5
coreyp2k <146997654+coreyp2k@users.noreply.github.com>
2023-11-07 20:13:11 -0500

    services upload

---
+++ servscript.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".header").addClass("active");
+        } else {
+           $(".header").removeClass("active");
+        }
+    });
+});

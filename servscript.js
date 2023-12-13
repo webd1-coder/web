bcb751d5e5db16c2463d3fea69dc02f6245e314e
coreyp2k <146997654+coreyp2k@users.noreply.github.com>
2023-12-13 15:36:08 -0500

    Delete servscript.js

--- servscript.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 50) {
-            $(".header").addClass("active");
-        } else {
-           $(".header").removeClass("active");
-        }
-    });
-});
